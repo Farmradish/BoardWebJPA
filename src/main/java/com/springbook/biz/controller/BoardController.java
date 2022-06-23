4021ab8a93d41e53b273eaf9c883ffe23b56deb3
farmradish <estell91234@gmail.com>
2022-06-24 01:14:14 +0900

    create project
    220624 create project

---
+++ src/main/java/com/springbook/biz/controller/BoardController.java
@@ -0,0 +1,17 @@
+package com.springbook.biz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+	
+	public BoardController() {
+		System.out.println("=> BoardController() Constructor");
+	}
+	
+	@GetMapping("/hello")
+	public String hello(String name) {
+		return "Hello: " + name;
+	}
+}
